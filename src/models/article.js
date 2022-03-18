45c4e6abdb3ba7fa69104e6a5c9307ab575c2689
Edson Cavalcante de Albuquerque Bisneto <51378539+edsonkz@users.noreply.github.com>
2022-03-18 17:14:27 -0300

    oldId agora não é unico e não pode ser mudado.

--- src/models/article.js
+++ src/models/article.js
@@ -7,36 +7,40 @@ const autoIncrement = AutoIncrementFactory(mongoose);
 var Schema = mongoose.Schema;
 
 const ArticleSchema = new Schema({
-    id: { type: Number, immutable: true, unique: true },
-    featured: { type: Boolean, required: true },
-    title: { type: String, required: true },
-    url: { type: String, required: true },
-    imageUrl: { type: String, required: true },
-    newsSite: { type: String, required: true },
-    summary: { type: String, required: isRequired },
-    publishedAt: { type: String, required: true },
-    launches: [{
-        _id: false,
-        id: String,
-        provider: String,
-    }, ],
-    events: [{
-        _id: false,
-        id: String,
-        provider: String,
-    }, ],
-    __v: { type: Number, select: false },
-    oldId: { type: Number, unique: true }
+	id: { type: Number, immutable: true, unique: true },
+	featured: { type: Boolean, required: true },
+	title: { type: String, required: true },
+	url: { type: String, required: true },
+	imageUrl: { type: String, required: true },
+	newsSite: { type: String, required: true },
+	summary: { type: String, required: isRequired },
+	publishedAt: { type: String, required: true },
+	launches: [
+		{
+			_id: false,
+			id: String,
+			provider: String,
+		},
+	],
+	events: [
+		{
+			_id: false,
+			id: String,
+			provider: String,
+		},
+	],
+	__v: { type: Number, select: false },
+	oldId: { type: Number, required: false, unique: false, immutable: true },
 });
 
 function isRequired() {
-    return typeof this.summary === "string" ? false : true;
+	return typeof this.summary === "string" ? false : true;
 }
 
-ArticleSchema.index({ publishedAt: 1 })
+ArticleSchema.index({ publishedAt: 1 });
 
 ArticleSchema.plugin(mongoosePaginate);
-ArticleSchema.plugin(autoIncrement, { inc_field: 'id' });
+ArticleSchema.plugin(autoIncrement, { inc_field: "id" });
 
 const Article = mongoose.model("Article", ArticleSchema);
 export { Article };
